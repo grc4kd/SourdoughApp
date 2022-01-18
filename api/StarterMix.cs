5bf769c2752981f340fd2c92179ba12953b86852
Griffin Crane <grc4kd@users.noreply.github.com>
2022-01-18 00:09:11 -0600

    Added core code and tests in F sharp.
    TODO, code not yet implemented

---
+++ api/StarterMix.cs
@@ -0,0 +1,28 @@
+namespace api;
+public class StarterMix
+{
+    // water as mass in grams
+    public int Water { get; set; }
+
+    // flour as mass in grams
+    public int Flour { get; set; }
+    
+    // total mass in grams
+    public int Mass => Water + Flour;
+
+    // TODO: calculate hydration dynamically
+    // public int Hydration => Ingredients(
+    //     Starter = 0, 
+    //     Flour, 
+    //     Water, 
+    //     Salt = 0);
+
+    // volume in milliliters
+    public int Volume { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
---
+++ tests/UnitTest1.fs
@@ -0,0 +1,68 @@
+module tests
+
+open NUnit.Framework
+open NUnit.Framework.Constraints
+
+open Ingredients
+
+[<SetUp>]
+let Setup () = ()
+
+[<Test>]
+let EqualityTestsWithTolerance () =
+    let testIngredientsActual = Components 289 1 0.68 1000
+    Assert.AreEqual(450.73, testIngredientsActual.Flour, 0.01)
+
+    Assert.That(
+        450.7,
+        Is
+            .EqualTo(testIngredientsActual.Flour)
+            .Within(0.05)
+    )
+
+    Assert.That(4.0, Is.Not.EqualTo(5.0).Within(0.5))
+    Assert.That(4.99f, Is.EqualTo(5.0f).Within(0.05f))
+    Assert.That(4.99m, Is.EqualTo(5.0m).Within(0.05m))
+    Assert.That(3999999999u, Is.EqualTo(4000000000u).Within(5u))
+    Assert.That(499, Is.EqualTo(500).Within(5))
+    Assert.That(4999999999L, Is.EqualTo(5000000000L).Within(5L))
+    Assert.That(5999999999UL, Is.EqualTo(6000000000UL).Within(5UL))
+
+[<Test>]
+let RunTests () =
+    Assert.That(
+        0.68,
+        Is
+            .EqualTo(Hydration 289.0 1.0 260.261905 450.738095)
+            .Within(0.000001)
+    )
+
+    let testIngredientsActual = Components 289 1 0.68 1000
+
+    Assert.That(
+        289,
+        Is
+            .EqualTo(testIngredientsActual.Starter)
+            .Within(0.000001)
+    )
+
+    Assert.That(
+        260.261905,
+        Is
+            .EqualTo(testIngredientsActual.Water)
+            .Within(0.000001)
+    )
+
+    Assert.That(
+        450.738095,
+        Is
+            .EqualTo(testIngredientsActual.Flour)
+            .Within(0.000001)
+    )
+
+    Assert.That(
+        9.09090,
+        Is
+            .EqualTo(testIngredientsActual.Salt)
+            .Within(0.000001)
+    )

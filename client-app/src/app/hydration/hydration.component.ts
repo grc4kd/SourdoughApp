802ee43da1ea43ae81ec3a3cbccf9737c160e03c
Griffin Crane <grc4kd@users.noreply.github.com>
2022-02-21 12:38:21 -0600

    created hydration component

---
+++ client-app/src/app/hydration/hydration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hydration',
+  templateUrl: './hydration.component.html',
+  styleUrls: ['./hydration.component.sass']
+})
+export class HydrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

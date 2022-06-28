b6cde7e5de559f55a1009aa59147a83fc001a695
Griffin Crane <grc4kd@users.noreply.github.com>
2022-06-28 00:05:59 -0500

    Make recipe unique. Some formatting changes.

--- tests/StarterMixTest.fs
+++ tests/StarterMixTest.fs
@@ -19,7 +19,7 @@ let Setup () = ()
 [<Test>]
 let PlainHydrationTest () =
     let starter: float<g> = 289.0<g>
-    let starterHydration: float = 1.0
+    let starterHydration: float = 1.00  // baker's math, 1 part flour : 1 part water => 100% == 1.00
     let water: float<g> = 260.261905<g>
     let flour: float<g> = 450.738095<g>
 
@@ -27,70 +27,52 @@ let PlainHydrationTest () =
 
 [<Test>]
 let PlainComponentsTest () =
-    let starter = 289.0
+    let starter = 289.0<g>
     let starterHydration = 1.0
     let desiredHydration = 0.68
-    let desiredMass = 1000.0
+    let desiredMass = 1000.0<g>
 
     let testIngredientsActual =
         Components starter starterHydration desiredHydration desiredMass
 
     Assert.That(testIngredientsActual.Starter, Is.EqualTo(289))
-
     Assert.That(testIngredientsActual.Water, Is.EqualTo(260.261904).Within(0.000001))
-
     Assert.That(testIngredientsActual.Flour, Is.EqualTo(450.738095).Within(0.000001))
-
     Assert.That(testIngredientsActual.Salt, Is.EqualTo(9.09090).Within(0.000001))
 
 [<Test>]
 let NewStarterTest () =
     Assert.AreEqual(1, NewStarter 100.0 100.0)
 
-// Website: http://breadandcompanatico.com/2013/10/16/extreme-country-sourdough-for-world-bread-day/
-// Ingredients for two large loaves: 428 g active sourdough starter with 120% hydration made
-// using organic stone-ground bread flour (protein: 10.5%), 700 g bread flour (protein: 12%),
-// 150 g organic stone-ground all-purpose flour (protein: 8.5%), 100 g organic whole-grain spelt
-// flour (mine was from sprouted spelt; protein: 13%), 640 g water + 50 to be added when you put
-// salt + 100 to add when you make the folds (but look at your dough before you add it); 20 grams
-// of sea salt. In total hydration was 85%.
+// Ingredients for two large loaves: 
+// 420 g active sourdough starter with 100% hydration made
+// 730 g bread flour, 225 g all-purpose flour
+// 620 g water + 40g to be added when you put
+// 100 g salt to add when you make the folds (but look at your dough before you add it); 20 grams of sea salt. 
+// In total hydration was about 85%.
 
 [<Test>]
 let ExtremeComponentTest () =
-    let starter = 428 // 428 g active sourdough starter
-    let starterHydration = 1.2 // with 120% hydration
-    let desiredHydration = 0.89420174741858627 // total hydration was 85% [misprint]
-    let desiredMass = 2168 // 428+700+150+100+640+50+100
-    // 20g of salt excluded, salt is calculator output only ATM
+    let starter = 420.0<g> // 420 g active sourdough starter
+    let starterHydration = 1.00 // with 100% hydration
+    let desiredHydration = 0.86254295532646053 // total hydration was about 85%
+    let desiredMass = 2155.0<g> // 420+730+225+620+40+100+20
+    // salt is always 90.90909% of total mass / 100 g
 
     let testIngredientsActual =
         Components starter starterHydration desiredHydration desiredMass
 
-    // 640 + 50 + 100
-    Assert.That(testIngredientsActual.Water, Is.EqualTo(790).Within(0.000001))
+    // 620 + 40
+    Assert.That(testIngredientsActual.Water, Is.EqualTo(787.97970479704782))
     // 700 + 150 + 100
-    Assert.That(testIngredientsActual.Flour, Is.EqualTo(950).Within(0.000001))
+    Assert.That(testIngredientsActual.Flour, Is.EqualTo(947.02029520295207))
 
 [<Test>]
 let ExtremeHydrationTest () =
-    let starter: float<g> = 428.0<g> // 428 g active sourdough starter
-    let starterHydration: float = 1.2 // with 120% hydration
+    let starter: float<g> = 428.0<g> // 420 g active sourdough starter
+    let starterHydration: float = 1.00 // with 100% hydration
     let water: float<g> = 640.0<g> + 50.0<g> + 100.0<g>
     let flour: float<g> = 700.0<g> + 150.0<g> + 100.0<g>
 
     let hydration: float = Hydration starter starterHydration water flour
-    Assert.That(hydration, Is.EqualTo(0.894201).Within(0.000001))
-
-[<Test>]
-let ConvertGramstoKilogramsTest () =
-    let grams = 2000.0<g>
-
-    Assert.That(ConvertGramsToKilograms(grams), Is.EqualTo(2<kg>))
-
-    let grams = 2.0<g>
-
-    Assert.That(ConvertGramsToKilograms(grams), Is.EqualTo(0.002<kg>))
-
-    let grams = 2342423454242.0<g>
-
-    Assert.That(ConvertGramsToKilograms(grams), Is.EqualTo(2342423454.242<kg>))
+    Assert.That(hydration, Is.EqualTo(0.86254295532646053))

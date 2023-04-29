4094b19acbce6e3b5d25850041f37d9b158cdee6
Griffin Crane <grc4kd@users.noreply.github.com>
2023-04-29 07:41:25 -0500

    use units of measure consistently in function

--- core/Ingredients.fs
+++ core/Ingredients.fs
@@ -26,8 +26,8 @@ let Components (starter: float<g>) starterHydration desiredHydration (desiredMas
     let sD: float<g> = starter / (starterHydration + 1.0)
     let hydrationComponent: float<g> = desiredHydration * sD
     let flour: float<g> = (desiredMass - sD - hydrationComponent) / (desiredHydration + 1.0)
-    let water = desiredMass - flour - starter
-    let salt = (desiredMass * 0.909090) / 100.0
+    let water: float<g> = desiredMass - flour - starter
+    let salt: float<g> = (desiredMass * 0.909090) / 100.0
     {
         Starter = starter
         Water = water

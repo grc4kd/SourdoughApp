8ecbde30e87ea1de8cb1c3f6c5c4cfc808cb1d42
Griffin <griffin.crane@outlook.com>
2023-11-16 23:12:08 -0600

    Reset changes by commit, then rebuilt client-app project.
    Signed-off-by: Griffin <griffin.crane@outlook.com>

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppComponent } from './app.component';
     MatButtonToggleModule,
     MatIconModule,
     MatToolbarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
